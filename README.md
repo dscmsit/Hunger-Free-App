50d6cc239e7e15381ef31cc88d7314c0a947b932
Aditya <adityasharma131003@gmail.com>
2023-03-15 18:26:38 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# no_hunger
+
+A flutter project 
---
+++ android/app/src/main/kotlin/com/gdscmsit/nohunger/no_hunger/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gdscmsit.nohunger.no_hunger
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
