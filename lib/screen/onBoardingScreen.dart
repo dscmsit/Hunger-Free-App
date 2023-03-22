723f5e56c9dfa2536d0a28b6f13ef0afa2b88da5
Home <praveen.sahu7512@gmail.com>
2023-03-22 02:07:11 -0400

    onBoardingScreen Added
    assest folder has been added for image resources...onboarding screen also added

---
+++ lib/screen/onBoardingScreen.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+
+class OnBoardingScreen extends StatefulWidget {
+  const OnBoardingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+
+  List onBoardingData = [
+    {
+      "image": 'assets/images/screen1.png',
+      "title": "Product 1",
+      "description": "Get The Food From Your NearBy"
+    },
+    {
+      "image": 'assets/images/screen2.png',
+      "title": "Product 2",
+      "description": "Contribute to the society"
+    },
+    {
+      "image":  'assets/images/screen3.png',
+      "title": "Product 3",
+      "description": "Enjoy the meal"
+    }
+  ];
+
+  PageController pageController = PageController();
+
+  void onPressed() {
+
+    //Yaha pe likhna hai kii konse page pe jaana hai
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    //this will remove the status bar
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [
+      SystemUiOverlay.bottom]);
+    return  Scaffold(
+      appBar: AppBar(
+
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        actions: [
+          TextButton(onPressed: onPressed, child: Text('Skip') )
+        ],
+      ),
+      
+      body: Stack(children: [
+        SizedBox(
+          height: 30,
+        ),
+        PageView.builder(scrollDirection: Axis.horizontal,
+          controller: pageController,
+          itemCount: onBoardingData.length,
+          itemBuilder: (context,index){
+            return Center(
+              child: Column(
+
+                children: [
+
+                  Image.asset(onBoardingData[index]['image']),
+                  Text(
+                    onBoardingData[index]['title'],
+                    style: TextStyle(fontSize: 20, color: Colors.blue),
+
+                  )
+                  ,
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    onBoardingData[index]['description'],
+                    style: TextStyle(fontSize: 20, color: Colors.blueAccent),
+
+                  )
+
+
+                ],
+
+
+              ),
+            );
+
+
+          },
+
+
+
+
+        )
+
+
+
+
+      ],),
+    );
+  }
+
+
+}
